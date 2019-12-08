14bc05fa9ba05cf9fb59b0d648cfd134d80a2d3d
AndroidLollipop <obfuscated00143241131643657478@gmail.com>
2019-12-08 16:24:26 +0800

    groundwork

--- react-callbag/todo-react-callbag/src/App.js
+++ react-callbag/todo-react-callbag/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Callbags from './callbags.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {hello: 5}
+  }
+
+  componentDidMount() {
+    Callbags.forEach((x) => this.setState({
+      hello: x
+    }))(Callbags.rangeInterval(1, 0, 1000))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            {this.state.hello}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
